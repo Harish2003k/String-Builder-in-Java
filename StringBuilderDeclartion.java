a471d3dce26ec2407fefeee1d1bd6d41258434a9
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-03 23:11:36 +0530

    Add files via upload
    commit

---
+++ StringBuilderDeclartion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class StringBuilderDeclartion {
+	public static void main(String args[]) {
+		StringBuilder sb =new StringBuilder("harish");
+		System.out.println(sb);
+	}
+
+}
