39e8da336db02f99f37ef455e90b992f6290a919
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-04 23:07:49 +0530

    Add files via upload
    commit

---
+++ StringBuilderReverse.java
@@ -0,0 +1,9 @@
+
+public class StringBuilderReverse {
+	public static void main(String args[]) {
+		StringBuilder sb=new StringBuilder("java is programming language.");
+		sb.reverse();
+		System.out.println(sb);
+	}
+
+}
