b0f2aee89c23b93403caf30f336d64388b9c61b8
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-04 22:58:16 +0530

    Add files via upload
    commit

---
+++ StringBuilderInsert.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class StringBuilderInsert {
+public static void main(String args[]) {
+	StringBuilder obj=new StringBuilder("Welcome to ");
+	obj.append("India");
+	System.out.println(obj);
+}
+}
